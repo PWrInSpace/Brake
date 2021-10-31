a55e6cc96e2ffe29e7acebbd08675d9531cbea9b
Kuba <74020427+JSiuda1@users.noreply.github.com>
2021-10-31 23:50:54 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,3 @@
-/*
-The sensor outputs provided by the library are the raw
-16-bit values obtained by concatenating the 8-bit high and
-low accelerometer and gyro data registers. They can be
-converted to units of g and dps (degrees per second) using
-the conversion factors specified in the datasheet for your
-particular device and full scale setting (gain).
-
-Example: An LSM6DS33 gives an accelerometer Z axis reading
-of 16276 with its default full scale setting of +/- 2 g. The
-LA_So specification in the LSM6DS33 datasheet (page 11)
-states a conversion factor of 0.061 mg/LSB (least
-significant bit) at this FS setting, so the raw reading of
-16276 corresponds to 16276 * 0.061 = 992.8 mg = 0.9928 g.
-*/
-
 #include <Arduino.h>
 #include <Wire.h>
 #include <ESP32Servo.h>
