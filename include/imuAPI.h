0a26548340d3ee1d096439537ddbf8f95edcae73
Kuba <74020427+JSiuda1@users.noreply.github.com>
2021-11-01 22:59:02 +0100

    Poprawa enum do rejestrów

--- include/imuAPI.h
+++ include/imuAPI.h
@@ -11,7 +11,9 @@ extern Errors errors;
 
 constexpr uint8_t AccReg[4] = {(0<<2), (1<<3), (1<<3) | (1<<2), (1<<2)};
 constexpr float AccFactor[4] = {0.061, 0.122, 0.244, 0.488};
-constexpr uint8_t BandWithReg[4] = {(0<<0), (1<<0), (1<<1), (1<<1) | (1<<0) };
+constexpr uint8_t GyroReg[4] = {};
+constexpr uint8_t BandWithReg[4] = {(1<<1) | (1<<0), (1<<1), (1<<0), (0<<0)};
+
 enum AccelerometerScale{
     A_2g = 0,
     A_4g,
@@ -21,16 +23,16 @@ enum AccelerometerScale{
 
 enum GyroscpoeScale{
     G_245dps = 0,
-    G_500dps = (1<<0),
-    G_1000dps = (1<<1),
-    G_2000dps = (1<<1) + (1<<0),
+    G_500dps,
+    G_1000dps,
+    G_2000dps,
 };
 
 enum BandWith{
-    B_50Hz = 0x11,
-    B_100Hz = 0x10,
-    B_200Hz = 0x01,
-    B_400Hz = 0x00,
+    B_50Hz = 0,
+    B_100Hz,
+    B_200Hz,
+    B_400Hz,
 };
 
 class ImuAPI{
